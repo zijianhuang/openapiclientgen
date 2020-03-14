ceacffce91c91c2a2d71dc43ae5f3c85d7a58f48
Z <zijianhuang2000@gmail.com>
2020-03-14 15:51:36 +1000

    init commit spinning of WebApiClientGen

---
+++ Fonlow.OpenApiClientGen.Abstract/ClientApiTsFunctionGenBase.cs
@@ -0,0 +1,17 @@
+﻿using Fonlow.Web.Meta;
+
+namespace Fonlow.CodeDom.Web.Ts
+{
+	public abstract class ClientApiTsFunctionGenBase : ClientApiTsFunctionGenAbstract
+	{
+		protected ClientApiTsFunctionGenBase():	base()
+		{
+
+		}
+
+		protected override string CreateUriQueryForTs(string uriText, ParameterDescription[] parameterDescriptions)
+		{
+			return UriQueryHelper.CreateUriQueryForTs(uriText, parameterDescriptions);
+		}
+	}
+}
