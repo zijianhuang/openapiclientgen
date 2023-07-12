3ea1dae8a1f8844cc2cdb0e4a649e3a6a8902f42
Kim <kim@ssi.co.nz>
2023-07-12 14:08:10 +1200

    Change to net7.0

--- Tests/DemoClientApi/CodeGen.bat
+++ Tests/DemoClientApi/CodeGen.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-..\..\Fonlow.OpenApiClientGen\bin\Debug\net6.0\Fonlow.OpenApiClientGen.exe pet.yaml petCodeGen.json
+..\..\Fonlow.OpenApiClientGen\bin\Debug\net7.0\Fonlow.OpenApiClientGen.exe pet.yaml petCodeGen.json
